42eb587b76995f1392f21fce3b33b9eac406252d
JT802 <jonathan.tresch@gmail.com>
2020-10-11 17:55:50 -0400

    Sunday evening work

--- main.js
+++ main.js
@@ -1,23 +1,22 @@
 // Global variables
 let boardState = ["", "", "", "", "", "", "", "", ""];
 let gameActive = true;
-let rePlay = document.getElementById("replay");
+let newGame = document.getElementById("newGame");
 let playerOne = "X";
 let playerTwo = "O";
 let computerPlayer = '';
 let currentPlayer = playerOne;
-let form = document.getElementById('form');
+let previousPlayer;
+let makePlayerName = document.getElementById("playerName");
+let submit = document.getElementById('submitButton');
 let onePlayerStart = document.getElementById("onePlayer");
 let twoPlayerStart = document.getElementById("twoPlayer");
-let playerVsPlayer = document.getElementById("twoPlayers");
-let playerVsComputer = document.getElementById("singlePlayer")
-//let whosUp = document.getElementById("whosUp");
+
+let whosUp = document.getElementById("whosUp");
 let provide = document.getElementById('provideStatus');
 let modeChoice;
 let timer;
 let winner; // !!! Should winner start as null? !!!
-let useClickedCell = document.querySelectorAll("cell").forEach(cells => cells.addEventListener("click", useClickedCell));
-// !!! Line 19 doesn't work. Need to write it differently !!!
 
 // Board Structure
 let cellZero = document.getElementById("cell-0");
@@ -30,17 +29,16 @@ let cellSix = document.getElementById("cell-6");
 let cellSeven = document.getElementById("cell-7");
 let cellEight = document.getElementById("cell-8");
 
-
 let clickedCells = [];
 let cellArray = [
-    cellZero, cellOne, cellTwo, cellFour, cellFive, cellSix, cellSeven, cellEight
+    cellZero, cellOne, cellTwo, cellThree, cellFour, cellFive, cellSix, cellSeven, cellEight
 ];
 
 
 // Getting by with a little help from const
-const itsWon = function() {`Player ${currentPlayer} is a winner!`};
-const isTied = function() {`It's a tie! Please start a new game.`};
-const onTurn = function() {`It's ${currentPlayer}'s turn.`};
+const itsWon = function () { `Player ${currentPlayer} is a winner!` };
+const isTied = function () { `It's a tie! Please start a new game.` };
+const onTurn = function () { `It's ${currentPlayer}'s turn.` };
 // Are lines 12-14 proper usage for template literals?
 // !!! Figure out if 41-43 are actually needed or can be changed !!!
 
@@ -60,88 +58,120 @@ const winConditions = {
     backSlash: [cellSix, cellFour, cellTwo]  // diagonal
 };
 
-function annouceWinner() {
+// !!! We need to do these things, but a bit differently
+
+
+/* --------------------- Player Buttons ----------------------- */
+
+onePlayerStart.addEventListener("click", () => {
+    onePlayerStart.disabled = true;
+    twoPlayerStart.disabled = true;
+    whosUp.textContent = currentPlayer;
+    start();
+})
+
+twoPlayerStart.addEventListener("click", () => {
+    onePlayerStart.disabled = true;
+    twoPlayerStart.disabled = true;
+    start();
+})
+
+// !!! This still isn't storing and displaying the player's name !!!
+submit.addEventListener('click', function (event) {
+    if (playerOne !== "") {
+        namePlayerOne(event)
+    } else {
+        namePlayerTwo(event)
+    }
+    event.preventDefault();
+    text = document.getElementById('playerName').value;
+}
+)
+
+function namePlayerOne(event) {
+    if (makePlayerName.value === "") {
+        playerOne = "X";
+        makePlayerName = "";
+    } else {
+        playerOne = makePlayerName.value;
+        makePlayerName = "";
+    }
+    console.log(playerOne + " is player one");
+
+}
+
+function namePlayerTwo(event) {
+    if (makePlayerName.value === "") {
+        playerTwo = "O";
+        makePlayerName = "";
+    } else {
+        playerTwo = makePlayerName.value;
+        makePlayerName = "";
+    }
+    console.log(playerTwo + " is player two");
+}
+
+// This should put the current player's click into the proper cell
+function useClickedCell(event) {
+    
+    if (event.target.textContent === "") {
+        if (currentPlayer === playerOne) {
+            event.target.textContent = "X";
+            clickedCells.push(event.target);
+            currentPlayer = playerTwo;
+            previousPlayer = playerOne;
+        } else if (currentPlayer === playerTwo) {
+            event.target.textContent = "O";
+            clickedCells.push(event.target);
+            currentPlayer = playerOne;
+            previousPlayer = playerTwo;
+        }
+    }
+    console.log(previousPlayer)
+    console.log(currentPlayer)
+    announceWinner();
+}
+
+function announceWinner() {
+    console.log("Inside announceWinner!")
     for (let combination of Object.values(winConditions)) {
         if (combination[0].textContent === "") {
         } else if (
             combination[0].textContent === combination[1].textContent &&
             combination[0].textContent === combination[2].textContent
         ) {
+            console.log("somebody won!")
             winner = true;
-            annouceWinner(combination);
-            provide.textContent = currentPlayer + " has WON!";
+            provide.textContent = previousPlayer + " has WON!";
+            
         }
-    }
-    isTied();
+    }   restartGame();
+    //isTied();
 
 
-} // !!! We need to do these things, but a bit differently
-
-// This should put the current player's click into the proper cell
-
-function useClickedCell(event) {
-    if (modeChoice === "playerVsPlayer") {
-        if (currentPlayer === playerOne) {
-            event.target.textContent = "X";
-            clickedCells.push(event.target);
-        } else if (currentPlayer === playerTwo) {
-            event.target.textContent = "O";
-            clickedCells.push(event.target);
-        }
-    } else if (modeChoice === 'playerVsComputer') {
-        if (currentPlayer === playerOne) {
-            event.target.textContent = "X";
-            clickedCells.push(event.target)
-            if (clickedCells.length < 9) {
-                computerGame()
-            }
-        } else if (currentPlayer === playerTwo) {
-            event.target.textContent = "O";
-            clickedCells.push(event.target);
-        }
-    }
-    //announceWinner();
-    //takePlayerChange();
 }
 
 // Provide ability to select the desired game (like in Guess the Number)
-function start(event, cellArray) {
-    cellArray.forEach(function (whatsClicked) {
-        whatsClicked.addEventListener("click", useClickedCell);
+function start() {
+    cellArray.forEach(function (clicky) {
+        clicky.addEventListener("click", useClickedCell);
     });
-    timer = setInterval(countItOut, 1000);
-    if (event.target.id === "playerVsPlayer") {
-        modeChoice = "playerVsPlayer";
+    //timer = setInterval(countItOut, 1000);
+    /*if (event.target.id === "twoPlayer") {
+        modeChoice = 2;
     } else {
-        modeChoice = "singlePlayer";
+        modeChoice = 1;
         playerTwo = computerPlayer;
-    }
+    } */
 }
 
-function computerGame() {
+/*function computerGame() {
     let computerClick = clickedCells[Math.floor(Math.random() * cellArray.length)];
     while (clickedCells.includes(computerClick)) {
         computerClick = cellArray[Math.floor(Math.random() * cellArray.length)]
     }
     computerClick.click()
-}
-/* --------------------- Player Buttons ----------------------- */
-
-onePlayerStart.addEventListener("click", () => {
-    onePlayerStart.disabled = true;
-    twoPlayerStart.disabled = true;
-})
-
-twoPlayerStart.addEventListener("click", () => {
-    onePlayerStart.disabled = true;
-    twoPlayerStart.disabled = true;
-})
-
-target.addEventListener('submit', function (event) { // !!! This still isn't storing and displaying the player's name !!!
-    event.preventDefault();
-    text = document.getElementById('playerName').value;
-    }
-)
+} */
 
 // This should allow for the game to swap between players
 
@@ -158,15 +188,17 @@ function takePlayerChange() {
 }
 
 // -------------- Restarts game, clears board, re-enables the start buttons, should allow for new player name input.
+
 function restartGame() {
-    gameActive = false;
-    currentPlayer = playerOne;
+    gameActive = true;
+    //currentPlayer = playerOne;
     boardState = ["", "", "", "", "", "", "", "", ""];
-    provide.textContent = takePlayerChange(); // !!! This doesn't look right !!!
+    //provide.textContent = takePlayerChange(); // !!! This doesn't look right !!!
     onePlayerStart.disabled = false;
     twoPlayerStart.disabled = false;
     document.querySelectorAll('cell').forEach(cell => cell.textContent = "");
-    document.querySelector("replay").addEventListener("click", restartGame);
+    //cellArray.removeEventListener("click")
+    newGame.addEventListener("click", clicky);
+}
     // !!! Having trouble with this. !!!
     /* !!! When restart button is clicked, it should re-enable onePlayerStart & twoPlayerStart, clear board, allow for new player name but doesn't. !!! */
-}
