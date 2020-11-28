cde22f31ecd7b20d99fdb07c58a05feeb3384eca
Andrea <andrea.vld.zrc@gmail.com>
2020-11-28 13:39:11 -0500

    continued with stories

--- JS/main.js
+++ JS/main.js
@@ -1,26 +1,26 @@
 // Global variables
 
 let gameActive = true;
-let newGame = document.getElementById("newGame");
+let resetGame = document.getElementById("newGame");
 let playerOne = "X";
 let playerTwo = "O";
 //let computerPlayer = '';
 let currentPlayer = playerOne;
-let previousPlayer;
+let previousPlayer = currentPlayer;
 let makePlayerName = document.getElementById("playerName");
-let submit = document.getElementById('submitButton');
+let submit = document.getElementById("submitButton");
 let onePlayerStart = document.getElementById("onePlayer");
 let twoPlayerStart = document.getElementById("twoPlayer");
 
 let whosUp = document.getElementById("whosUp");
-let gameWinner = document.getElementById('winStatus');
+let gameWinner = document.getElementById("winStatus");
 
 // Lines 20-23 deal with story(ies) that don't yet work
 let seconds = 0;
-let modeChoice; 
+let modeChoice;
 let timer;
 let winner;
-let elapsedTime = document.getElementById("time-elapsed")
+let elapsedTime = document.getElementById("time-elapsed");
 // ______________________________________________
 
 // Board Structure
@@ -36,183 +36,219 @@ let cellNine = document.getElementById("cell-9");
 
 let clickedCells = [];
 let cellArray = [
-    cellOne, cellTwo, cellThree, cellFour, cellFive, cellSix, cellSeven, cellEight, cellNine
+  cellOne,
+  cellTwo,
+  cellThree,
+  cellFour,
+  cellFive,
+  cellSix,
+  cellSeven,
+  cellEight,
+  cellNine,
 ];
 
 // This shows that a tie condition has been met
-const isTied = function () { `It's a tie! Please start a new game.` };
+const isTied = function () {
+  `It's a tie! Please start a new game.`;
+};
 
 // This *should* create the board
-const cells = document.querySelectorAll('.cell');
+const cells = document.querySelectorAll(".cell");
 // provide.innerHTML = onturn();
 
 // These win conditions have remained so throughout the ages. Anyone hear Gregorian chanting?
 
 const winConditions = {
-    rowOne: [cellOne, cellTwo, cellThree], // row
-    rowTwo: [cellFour, cellFive, cellSix], // row
-    rowThree: [cellSeven, cellEight, cellNine], // row
-    columnOne: [cellOne, cellFour, cellSeven], // column
-    columnTwo: [cellTwo, cellFive, cellEight], // column
-    columnThree: [cellThree, cellSix, cellNine], // column
-    forwardSlash: [cellOne, cellFive, cellNine], // diagonal
-    backSlash: [cellSeven, cellFive, cellThree]  // diagonal
+  rowOne: [cellOne, cellTwo, cellThree], // row
+  rowTwo: [cellFour, cellFive, cellSix], // row
+  rowThree: [cellSeven, cellEight, cellNine], // row
+  columnOne: [cellOne, cellFour, cellSeven], // column
+  columnTwo: [cellTwo, cellFive, cellEight], // column
+  columnThree: [cellThree, cellSix, cellNine], // column
+  forwardSlash: [cellOne, cellFive, cellNine], // diagonal
+  backSlash: [cellSeven, cellFive, cellThree], // diagonal
 };
 
 /* --------------------- Button Event Listeners----------------------- */
 
 onePlayerStart.addEventListener("click", (event) => {
-    onePlayerStart.disabled = true;
-    twoPlayerStart.disabled = true;
-    whosUp.textContent = currentPlayer;
-    start(event);
-})
+  onePlayerStart.disabled = true;
+  twoPlayerStart.disabled = true;
+  whosUp.textContent = currentPlayer;
+  start(event);
+});
 
 twoPlayerStart.addEventListener("click", (event) => {
-    onePlayerStart.disabled = true;
-    twoPlayerStart.disabled = true;
-    whosUp.textContent = currentPlayer;
-    start(event);
-})
+  onePlayerStart.disabled = true;
+  twoPlayerStart.disabled = true;
+  whosUp.textContent = currentPlayer;
+  start(event);
+});
 
-submit.addEventListener('click', function (event) {
-    if (playerOne !== "") {
-        namePlayerOne(event)
-    } else {
-        namePlayerTwo(event)
-    }
-    event.preventDefault();
-    text = document.getElementById('playerName').value;
-})
+submit.addEventListener("click", function (event) {
+  if (playerOne === "X") {
+    namePlayerOne(event);
+  } else if (playerTwo === "") {
+    namePlayerTwo(event);
+  }
+  // event.preventDefault();
+  // text = document.getElementById('playerName').value;
+});
 
-//newGame.addEventListener("click", eachCell);
+resetGame.addEventListener("click", function (event) {
+  onePlayerStart.disabled = false;
+  twoPlayerStart.disabled = false;
+  resetBoard(event, cellArray)
+});
 
 // Provides ability to select the desired game (like in Guess the Number)
 function start(event) {
-    cellArray.forEach(function (eachCell) {
-        eachCell.addEventListener("click", useClickedCell);
-    });
-    //timer = setInterval(countItOut, 1000);
-    //seconds ++
-    //elapsedTime.textContent = seconds + " have elapsed.";
-    console.log(event.target);
-    if (event.target.id === "twoPlayer") {
-        modeChoice = "twoPlayer";
-    } else {
-        modeChoice = "onePlayer";
-    }
-} 
+  cellArray.forEach(function (eachCell) {
+    eachCell.addEventListener("click", useClickedCell);
+  });
+  timer = setInterval(gameTimer, 1000);
+  
+  console.log(event.target);
+  if (event.target.id === "twoPlayer") {
+    modeChoice = "twoPlayer";
+  } else {
+    modeChoice = "onePlayer";
+    playerTwo = 'computer';
+  } 
+}
 
 // Restarts game, clears board, re-enables the start buttons, should allow for new player name input.
 function stopGame(cellArray) {
-    cellArray.forEach(function (cell) {
-        cell.removeEventListener("click", useClickedCell)
-    })
-    onePlayerStart.disabled = false;
-    twoPlayerStart.disabled = false;
+  cellArray.forEach(function (cell) {
+    cell.removeEventListener("click", useClickedCell);
+  });
+  
 }
 
-/*function computerGame() {
-    let computerClick = clickedCells[Math.floor(Math.random() * cellArray.length)];
-    while (clickedCells.includes(computerClick)) {
+function resetBoard(cellArray){
+cellArray.forEach(function (cell) {
+    cell.textContent = ''
+})
+}
+
+function computerPlayer() {
+ let computerClick = cellArray[Math.floor(Math.random() * cellArray.length)];
+   while (clickedCells.includes(computerClick)) {
         computerClick = cellArray[Math.floor(Math.random() * cellArray.length)]
     }
     computerClick.click()
-} */
+}
 
 // This should put the current player's click into the proper cell
 function useClickedCell(event) {
-    if (modeChoice === "twoPlayer") {
-        if (currentPlayer === playerOne) {
-            event.target.textContent = "X";
-            clickedCells.push(event.target);
-        } else if (currentPlayer === playerTwo) {
-            event.target.textContent = "O";
-            clickedCells.push(event.target);
-        }
-    } else if (modeChoice === "onePlayer") {
-        if (currentPlayer === playerOne) {
-            event.target.textContent = "X";
-            clickedCells.push(event.target);
-        } else if (currentPlayer === playerTwo) {
-            event.target.textContent = "O";
-            clickedCells.push(event.target);
-        }   
+  if (modeChoice === "twoPlayer") {
+    if (currentPlayer === playerOne) {
+      event.target.textContent = "X";
+      clickedCells.push(event.target);
+    } else if (currentPlayer === playerTwo) {
+      event.target.textContent = "O";
+      clickedCells.push(event.target);
     }
-    switchPlayer();
-    removeClickedCell(event);
-    announceWinner();
+  } else if (modeChoice === "onePlayer") {
+    if (currentPlayer === playerOne) {
+      event.target.textContent = "X";
+      clickedCells.push(event.target);
+    } else if (currentPlayer === playerTwo) {
+      computerPlayer();
+      event.target.textContent = "O";
+      clickedCells.push(event.target);
+      
+    }
+  }
+  switchPlayer();
+  removeClickedCell(event);
+  announceWinner();
 }
 
 // function for switching the player turn
 function switchPlayer() {
-    if (currentPlayer === playerOne) {
-        currentPlayer = playerTwo;
-        whosUp.textContent = currentPlayer;
-    } else if (currentPlayer === playerTwo) {
-        currentPlayer = playerOne;
-        whosUp.textContent = currentPlayer;
-    }
+  if (currentPlayer === playerOne) {
+    currentPlayer = playerTwo;
+    whosUp.textContent = currentPlayer;
+  } else if (currentPlayer === playerTwo) {
+    currentPlayer = playerOne;
+    whosUp.textContent = currentPlayer;
+  }
 }
 
 // function to prevent overwriting an already-filled cell and tell player to pick a different cell
 function removeClickedCell(event) {
-    event.target.removeEventListener("click", useClickedCell);
-    event.target.addEventListener("click", () => {
-        alert("Please click a different cell!")
-    })
+  event.target.removeEventListener("click", useClickedCell);
+  event.target.addEventListener("click", () => {
+    alert("Please click a different cell!");
+  });
 }
 
 // This will announce the winner when a win condition is met
 function announceWinner() {
-    console.log("Inside announceWinner!")
-    for (let combination of Object.values(winConditions)) {
-        if (combination[0].textContent === "") {
-        } else if (
-            combination[0].textContent === combination[1].textContent &&
-            combination[0].textContent === combination[2].textContent
-        ) {
-            winner = true;
-            showWinner();
-            gameWinner.textContent = previousPlayer + " has WON!";
-            stopGame(cellArray);
-        }
-    }
-    //isTied();
+  for (let combination of Object.values(winConditions)) {
+    if (combination[0].textContent === "") {
+    } else if (
+      combination[0].textContent === combination[1].textContent &&
+      combination[0].textContent === combination[2].textContent
+    ) {
+      winner = true;
+      showWinner(combination);
+      console.log(combination);
 
+      gameWinner.textContent = previousPlayer + " has WON!";
+      console.log(previousPlayer);
+      stopGame(cellArray);
+      stopGameTimer()
+    }
+  }
+  //isTied();
 }
 
 // Function to highlight the winning condition
-function showWinner(winArray) {
-    winArray.forEach(function(winCells) {
-        winCells.className = "winner";
-    })
+function showWinner(combination) {
+  combination.forEach(function (winConditions) {
+    winConditions.className = "winner";
+  });
 }
 
 // function to show a tie condition has been met ...
 
-
 // This will (eventually) allow the player name entered to be tracked
-function namePlayerOne(event) {
-    if (makePlayerName.value === "") {
-        playerOne = "X";
-        makePlayerName = "";
-    } else {
-        playerOne = makePlayerName.value;
-        makePlayerName = "";
-    }
+function namePlayerOne() {
+  if (makePlayerName.value === "") {
+    playerOne = "X";
+  } else {
+    playerOne = makePlayerName.value;
+    // makePlayerName = "";
+    currentPlayer = playerOne;
     console.log(playerOne + " is player one");
-
+  }
 }
 
-function namePlayerTwo(event) {
-    if (makePlayerName.value === "") {
-        playerTwo = "O";
-        makePlayerName = "";
-    } else {
-        playerTwo = makePlayerName.value;
-        makePlayerName = "";
-    }
+function namePlayerTwo() {
+  if (makePlayerName.value === "") {
+    playerTwo = "O";
+  } else {
+    playerTwo = makePlayerName.value;
+    // makePlayerName = "";
+
     console.log(playerTwo + " is player two");
+  }
 }
 
+// this function starts the game timer at the start of a new game
+function gameTimer() {
+  seconds += 1;
+  if (seconds < 10) {
+    elapsedTime.textContent = "0" + seconds + " has elapsed";
+  } else {
+    elapsedTime.textContent = seconds;
+  }
+  
+}
+
+// this function stops the game timer when a game ends
+function stopGameTimer(){
+  clearInterval(timer)  
+}
